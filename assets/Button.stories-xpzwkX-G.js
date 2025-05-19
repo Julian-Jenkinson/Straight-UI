0c836184e1b4e18dbc276f306e7bd2ec3709c539
catc4ke <julianjenkinson@hotmail.com>
2025-05-19 13:00:58 +1000

    Updates

--- assets/Button.stories-xpzwkX-G.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-D_zvdyIk.js";const c=({children:s,onClick:n})=>a.jsx("button",{onClick:n,children:s}),i={title:"Components/Button",component:c,tags:["autodocs"]},r={args:{children:"Click Me"}};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'Click Me'
-  }
-}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const p=["Primary"];export{r as Primary,p as __namedExportsOrder,i as default};
