d7b18a9038014afdd33debf3c9bab0dfc0d2d8c9
catc4ke <julianjenkinson@hotmail.com>
2025-05-19 12:46:21 +1000

    Updates

--- assets/Button.stories-CrI4spIX.js
+++
@@ -1,5 +0,0 @@
-import{j as c}from"./jsx-runtime-D_zvdyIk.js";const r=({children:t,onClick:s})=>c.jsx("button",{onClick:s,children:t});try{r.displayName="Button",r.__docgenInfo={description:"",displayName:"Button",props:{onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"() => void"}}}}}catch{}const d={title:"Components/Button",component:r,tags:["autodocs"]},e={args:{children:"Click Me"}};var o,n,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: 'Click Me'
-  }
-}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const p=["Primary"];export{e as Primary,p as __namedExportsOrder,d as default};
