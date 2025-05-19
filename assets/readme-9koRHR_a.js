963748321fc362045ef32654eb2ef9b544fa8013
catc4ke <julianjenkinson@hotmail.com>
2025-05-19 13:54:38 +1000

    deploy storybook

---
+++ assets/readme-9koRHR_a.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-B8rYRX2K.js";import{M as i}from"./index-hd5gCCt5.js";import"./index-D4lIrffr.js";import"./iframe-Z11FPZx9.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function o(n){const e={code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Documentation/README"}),`
+`,t.jsx(e.h1,{id:"-project-overview",children:"📘 Project Overview"}),`
+`,t.jsxs(e.p,{children:["Welcome to the documentation for ",t.jsx(e.strong,{children:"Straight-UI"}),"!"]}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"This library is a collection of reusable UI components."}),`
+`,t.jsx(e.li,{children:"Built with React + Vite + Storybook 8."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"-getting-started",children:"🚀 Getting Started"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-bash",children:`npm install straight-ui
+`})})]})}function j(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{j as default};
